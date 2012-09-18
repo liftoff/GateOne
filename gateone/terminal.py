9eb7c6a857c8b2eb2360433096e64c343c5602da
Dan McDougall <daniel.mcdougall@liftoffsoftware.com>
2012-09-17 21:37:54 -0400

    terminal.py:  Fixed Terminal.insert_characters() hopefully for the last time.  Sigh.

--- gateone/terminal.py
+++ gateone/terminal.py
@@ -2057,8 +2057,8 @@ class Terminal(object):
         #logging.debug("insert_characters(%s)" % n)
         n = int(n)
         for i in xrange(n):
-            self.screen[self.cursorY].pop(self.cursorX + i)
-            self.screen[self.cursorY].insert(self.cursorX + i, u' ')
+            self.screen[self.cursorY].pop() # Take one down, pass it around
+            self.screen[self.cursorY].insert(self.cursorX, u' ')
 
     def delete_characters(self, n=1):
         """
