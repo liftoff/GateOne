bed840f06762080802e0bdca3571f75710249be2
Dan McDougall <daniel.mcdougall@liftoffsoftware.com>
2012-10-13 17:25:49 -0400

    gateone.js:  Added GateOne.Utils.getCookie(*name*).  Does exactly what you'd expect. gateone.js:  Added GateOne.Utils.setCookie(*name*, *value*, *days*).  Also does what you'd expect. gateone.js:  Adjusted pastearea.onmousemove to better handle the situation where a link is under the cursor (it had some issues when the element under the cursor was inside of an anchor tag). auth.py:  Added a little fix for an edge case where the user's cookie provides a valid user but the client submitted completely different 'auth' data.  This situation will no longer result in the user ending up with a new session every time they connect. Documentation:  Added the getCookie() and setCookie() functions to the gateone.js docs. Logging Plugin:  Removed a duplicate line.

--- gateone/auth.py
+++ gateone/auth.py
@@ -149,7 +149,8 @@ class NullAuthHandler(BaseAuthHandler):
             # This lets any origin check if the user has been authenticated
             # (necessary to prevent "not allowed ..." XHR errors)
             self.set_header('Access-Control-Allow-Origin', '*')
-            self.user_login(user)
+            if not self.get_current_user():
+                self.user_login(user)
             self.write('authenticated')
             self.finish()
             return
