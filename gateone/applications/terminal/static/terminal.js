2b79adf07b07bc609a4bf6f8fafcca189a05f5ba
Dan McDougall <daniel.mcdougall@liftoffsoftware.com>
2013-10-06 11:56:23 -0400

    terminal.js:  Fixed the bug where terminals were not being auto-scrolled to the bottom of the view when switching workspaces.

--- gateone/applications/terminal/static/terminal.js
+++ gateone/applications/terminal/static/terminal.js
@@ -1971,6 +1971,7 @@ go.Base.update(GateOne.Terminal, {
             setActivityCheckboxes(term);
             setEncodingValue(term);
             setKeyboardValue(term);
+            u.scrollToBottom(termNode);
         } else {
             return; // This can happen if the terminal closed before a timeout completed.  Not a big deal, ignore
         }
