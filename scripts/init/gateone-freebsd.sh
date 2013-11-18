819e93d6bc908a506971366c1f7c21c9a62bea8b
gr0undzer0 <christopher.donald.jones@gmail.com>
2013-11-17 21:49:25 -0500

    Added a freeBsd init script.

---
+++ scripts/init/gateone-freebsd.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# PROVIDE: gateone 
+# REQUIRE: LOGIN cleanvar sshd
+# KEYWORD: shutdown
+
+. /etc/rc.subr
+
+name=gateone
+rcvar=gateone_enable
+command=/usr/local/bin/gateone 
+command_interpreter=/usr/local/bin/python
+gateone_user=gateone
+start_cmd="/usr/sbin/daemon $command"
+load_rc_config $name
+run_rc_command "$1"
