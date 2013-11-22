41fada8588d84e4ba75fe5f378e2aa61b6f4ff9b
Dan McDougall <daniel.mcdougall@liftoffsoftware.com>
2013-11-22 08:46:04 -0500

    Added missing policy.py file that I forgot to 'git add' last night.

---
+++ gateone/applications/terminal/policy.py
@@ -0,0 +1,198 @@
+# -*- coding: utf-8 -*-
+#
+#       Copyright 2013 Liftoff Software Corporation
+#
+from __future__ import unicode_literals
+
+__doc__ = """\
+policy.py - A module containing the Terminal application's policy functions.
+"""
+
+# Meta
+__license__ = "AGPLv3 or Proprietary (see LICENSE.txt)"
+__author__ = 'Dan McDougall <daniel.mcdougall@liftoffsoftware.com>'
+
+from functools import partial
+from gateone import SESSIONS
+from gateone.core.locale import get_translation
+from gateone.core.log import go_logger
+from gateone.auth.authorization import applicable_policies
+
+term_log = go_logger("gateone.terminal")
+
+# Localization support
+_ = get_translation()
+
+def policy_new_terminal(cls, policy):
+    """
+    Called by :func:`terminal_policies`, returns True if the user is
+    authorized to execute :func:`new_terminal` and applies any configured
+    restrictions (e.g. max_dimensions).  Specifically, checks to make sure the
+    user is not in violation of their applicable policies (e.g. max_terms).
+    """
+    instance = cls.instance
+    session = instance.ws.session
+    auth_log = instance.ws.auth_log
+    try:
+        term = cls.f_args[0]['term']
+    except (KeyError, IndexError):
+        # new_terminal got bad *settings*.  Deny
+        return False
+    user = instance.current_user
+    open_terminals = 0
+    locations = SESSIONS[session]['locations']
+    if term in instance.loc_terms:
+        # Terminal already exists (reattaching) or was shared by someone else
+        return True
+    for loc in locations.values():
+        for t, term_obj in loc['terminal'].items():
+            if t in instance.loc_terms:
+                if user == term_obj['user']:
+                    # Terms shared by others don't count
+                    if user['upn'] == 'ANONYMOUS':
+                        # ANONYMOUS users are all the same so we have to use
+                        # the session ID
+                        if session == term_obj['user']['session']:
+                            open_terminals += 1
+                    else:
+                        open_terminals += 1
+    # Start by determining the limits
+    max_terms = 0 # No limit
+    if 'max_terms' in policy:
+        max_terms = policy['max_terms']
+    max_cols = 0
+    max_rows = 0
+    if 'max_dimensions' in policy:
+        max_cols = policy['max_dimensions']['columns']
+        max_rows = policy['max_dimensions']['rows']
+    if max_terms:
+        if open_terminals >= max_terms:
+            auth_log.error(_(
+                "%s denied opening new terminal.  The 'max_terms' policy limit "
+                "(%s) has been reached for this user." % (
+                user['upn'], max_terms)))
+            # Let the client know this term is no more (after a timeout so the
+            # can complete its newTerminal stuff beforehand).
+            term_ended = partial(instance.term_ended, term)
+            instance.add_timeout("500", term_ended)
+            cls.error = _(
+                "Server policy dictates that you may only open %s terminal(s) "
+                % max_terms)
+            return False
+    if max_cols:
+        if int(cls.f_args['columns']) > max_cols:
+            cls.f_args['columns'] = max_cols # Reduce to max size
+    if max_rows:
+        if int(cls.f_args['rows']) > max_rows:
+            cls.f_args['rows'] = max_rows # Reduce to max size
+    return True
+
+def policy_share_terminal(cls, policy):
+    """
+    Called by :func:`terminal_policies`, returns True if the user is
+    authorized to execute :func:`share_terminal`.
+    """
+    auth_log = cls.instance.ws.auth_log
+    user = cls.instance.current_user
+    try:
+        cls.f_args[0]['term']
+    except (KeyError, IndexError):
+        # share_terminal got bad *settings*.  Deny
+        return False
+    can_share = policy.get('share_terminals', True)
+    if not can_share:
+        auth_log.error(_(
+            "%s denied sharing terminal by policy." % user['upn']))
+        return False
+    return True
+
+def policy_char_handler(cls, policy):
+    """
+    Called by :func:`terminal_policies`, returns True if the user is
+    authorized to write to the current (or specified) terminal.
+    """
+    error_msg = _("You do not have permission to write to this terminal.")
+    cls.error = error_msg
+    instance = cls.instance
+    try:
+        term = cls.f_args[1]
+    except IndexError:
+        # char_handler didn't get 'term' as a non-keyword argument.  Try kword:
+        try:
+            term = cls.f_kwargs['term']
+        except KeyError:
+            # No 'term' was given at all.  Use current_term
+            term = instance.current_term
+    # Make sure the term is an int
+    term = int(term)
+    if term not in instance.loc_terms:
+        return True # Terminal was probably just closed
+    term_obj = instance.loc_terms[term]
+    user = instance.current_user
+    if user['upn'] == term_obj['user']['upn']:
+        # UPN match...  Double-check ANONYMOUS
+        if user['upn'] == 'ANONYMOUS':
+            # All users will be ANONYMOUS so we need to check their session ID
+            if user['session'] == term_obj['user']['session']:
+                return True
+        # TODO: Think about adding an administrative lock feature here
+        else:
+            return True # Users can always write to their own terminals
+    if 'share_id' in term_obj:
+        # This is a shared terminal.  Check if the user is in the 'write' list
+        shared = instance.ws.persist['terminal']['shared']
+        share_obj = shared[term_obj['share_id']]
+        if user['upn'] in share_obj['write']:
+            return True
+        elif share_obj['write'] in ['AUTHENTICATED', 'ANONYMOUS']:
+            return True
+        elif isinstance(share_obj['write'], list):
+            # Iterate and check each item
+            for allowed in share_obj['write']:
+                if allowed == user['upn']:
+                    return True
+                elif allowed in ['AUTHENTICATED', 'ANONYMOUS']:
+                    return True
+        # TODO: Handle regexes and lists of regexes here
+    return False
+
+def terminal_policies(cls):
+    """
+    This function gets registered under 'terminal' in the
+    :attr:`ApplicationWebSocket.security` dict and is called by the
+    :func:`require` decorator by way of the :class:`policies` sub-function. It
+    returns True or False depending on what is defined in the settings dir and
+    what function is being called.
+
+    This function will keep track of and place limmits on the following:
+
+        * The number of open terminals.
+        * How big each terminal may be.
+        * Who may view or write to a shared terminal.
+
+    If no 'terminal' policies are defined this function will always return True.
+    """
+    instance = cls.instance # TerminalApplication instance
+    function = cls.function # Wrapped function
+    #f_args = cls.f_args     # Wrapped function's arguments
+    #f_kwargs = cls.f_kwargs # Wrapped function's keyword arguments
+    policy_functions = {
+        'new_terminal': policy_new_terminal,
+        'share_terminal': policy_share_terminal,
+        'char_handler': policy_char_handler
+    }
+    auth_log = instance.ws.auth_log
+    user = instance.current_user
+    policy = applicable_policies('terminal', user, instance.ws.prefs)
+    if not policy: # Empty RUDict
+        return True # A world without limits!
+    # Start by determining if the user can even login to the terminal app
+    if 'allow' in policy:
+        if not policy['allow']:
+            auth_log.error(_(
+                "%s denied access to the Terminal application by policy."
+                % user['upn']))
+            return False
+    if function.__name__ in policy_functions:
+        return policy_functions[function.__name__](cls, policy)
+    return True # Default to permissive if we made it this far
--- gateone/static/gateone.js
+++ gateone/static/gateone.js
@@ -80,7 +80,7 @@ The base object for all Gate One modules/plugins.
 */
 GateOne.__name__ = "GateOne";
 GateOne.__version__ = "1.2";
-GateOne.__commit__ = "20131121213705";
+GateOne.__commit__ = "20131121220213";
 GateOne.__repr__ = function () {
     return "[" + this.__name__ + " " + this.__version__ + "]";
 };
