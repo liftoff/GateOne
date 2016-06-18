b24de28f40b729aa427d6d34d3a90e901c539533
Dan McDougall <daniel.mcdougall@liftoffsoftware.com>
2016-06-18 14:06:17 -0400

    Rebuilt the docs (again) because the new sphinx default theme looks terrible.  Using RTD theme now.  Also removed the huge logo.

--- gateone/docs/source/conf.py
+++ gateone/docs/source/conf.py
@@ -168,7 +168,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -187,7 +187,7 @@ html_short_title = "Gate One Documentation"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'Images/ls_logo_1inch_300dpi.png'
+#html_logo = 'Images/ls_logo_1inch_300dpi.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
